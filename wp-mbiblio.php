f136851933544711f2d3c82958ee8084379e4442
Samir Husaj <Samir Husaj>
2015-10-29 16:35:48 +0100

    Initial commit

---
+++ wp-mbiblio.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: ZHAW - ZPP Modellbibliothek
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Webapplikation für die Modellbibliothek an der ZHAW (School of Engineering) ZPP
+Version: 0.1
+Author: Samir Husaj
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+?>
+
+
