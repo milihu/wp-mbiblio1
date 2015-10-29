bad6f1c5fd4f4cdb0abebfa91338ddd0e63033f6
Samir Husaj <samir_hu@yahoo.de>
2015-10-29 22:32:34 +0100

    created : readme.txt uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samir Husaj
+ * Date: 29.10.2015
+ * Time: 19:49
+ */
+// uninstall.php
+
+
+/**
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+if ( ! WP_UNINSTALL_PLUGIN ) {
+	exit();
+}
+
+// entferne etwas
+*/
+
+?>
